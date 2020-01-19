b688c0d689fca1723fd0cb2e9eebf958c34183ca
Arman Mottaghi <me@r--man.com>
2020-01-19 11:50:58 -0800

    removed ga

--- background.js
+++ background.js
@@ -343,21 +343,6 @@ var slowSwitchActiveUsage = function() {
 	}
 }
 
-//check for actual active users
-var _AnalyticsCode = 'UA-51920707-1';
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', _AnalyticsCode]);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = 'https://ssl.google-analytics.com/ga.js';
-	//ga.src = 'https://ssl.google-analytics.com/u/ga_debug.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
-})();
 document.addEventListener('DOMContentLoaded', function () {
 	domLoaded = true;
 
